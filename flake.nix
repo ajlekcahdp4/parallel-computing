18ecaf47a46a0aff89e841269a50a0f46a4f4874
AJlEKCAHDP_lV <alex.rom23@mail.ru>
2024-03-10 19:24:11 +0300

    [MISC] initial commit

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    lint-nix.url = "github:xc-jp/lint.nix";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs: let
+    systems = ["x86_64-linux" "aarch64-linux"];
+  in
+    flake-utils.lib.eachSystem systems (system: let
+      pkgs = import nixpkgs {inherit system;};
+
+      clang-tools = pkgs.clang-tools_17;
+      lints = (import ./nix/lints.nix {inherit inputs;}) pkgs ./.;
+      nativeBuildInputs = with pkgs; [
+        clang-tools
+        act
+        just
+        mpi
+        automake
+        autoconf
+        addlicense
+        gdb
+        gnuplot
+        bc
+      ];
+
+      shell =
+        (pkgs.mkShell.override {
+          stdenv = pkgs.gcc49Stdenv;
+        }) {
+          inherit nativeBuildInputs;
+        };
+    in {
+      devShells = {
+        default = shell;
+        devGcc = shell;
+      };
+    });
+}
